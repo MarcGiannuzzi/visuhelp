9d880c3e6fbdd26b3090cf72d3035ff7c83f687c
Marc <marc.giannuzzi@gmail.com>
2020-06-12 18:19:40 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          VisuHelp app for all data scientists accross the world.
         </p>
         <a
           className="App-link"
